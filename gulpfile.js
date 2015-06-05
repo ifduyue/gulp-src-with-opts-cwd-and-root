caeacf988979bbbf0b33687f2f9cd9f0e572d5e5
Yue Du <ifduyue@gmail.com>
2015-06-06 00:53:21 +0800

    demo for wearefractal/glob-stream#42

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    es = require('event-stream'),
+    reduce = require("stream-reduce"),
+    gutil = require('gulp-util'),
+    objectAssign = require('object-assign');
+
+var files = ['a', '/b', 'c/d', '/c/e'];
+var opts = [{'cwd' : 'this-is-root'},
+            {'root': 'this-is-root'},
+            {'cwd' : 'this-is-root',
+             'root': 'this-is-root'}];
+
+gulp.task('default', function() {
+    var streams = [];
+    opts.forEach(function(opt) {
+        streams.push(gulp.src(files, objectAssign({}, opt))
+            .pipe(reduce(function(acc, file) {
+                return acc.concat([file.relative]);
+            }, []).on('data', function(data) {
+                gutil.log(opt, 'captures', data);
+            })));
+    });
+    es.merge.apply(this, streams);
+});
